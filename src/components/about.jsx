f9ec4607623bb68911bd21f8de1459ba4e8bc778
Clement Marcilhacy <clementmarcilhacy@gmail.com>
2022-01-18 22:40:34 +0100

    initial commit with changes applied to react-landing-page-template

---
+++ src/components/about.jsx
@@ -0,0 +1,47 @@
+export const About = (props) => {
+  return (
+    <div id="about">
+      <div className="container">
+        <div className="row">
+          <div className="col-xs-12 col-md-6">
+            {" "}
+            <img
+              src="img/carousel-3-resized.jpg"
+              className="img-responsive"
+              alt=""
+            />{" "}
+          </div>
+          <div className="col-xs-12 col-md-6">
+            <div className="about-text">
+              <h2>A propos</h2>
+              <p>{props.data ? props.data.paragraph : "loading..."}</p>
+              <h3>
+                Les brûleurs Sparxs sont disponibles sous différentes formes:
+              </h3>
+              <div className="list-style">
+                <div className="col-lg-12 col-sm-12 col-xs-12">
+                  <ul>
+                    {props.data
+                      ? props.data.Why.map((d, i) => (
+                          <li key={`${d}-${i}`}>{d}</li>
+                        ))
+                      : "loading"}
+                  </ul>
+                </div>
+                {/* <div className="col-lg-6 col-sm-6 col-xs-12">
+                  <ul>
+                    {props.data
+                      ? props.data.Why2.map((d, i) => (
+                          <li key={`${d}-${i}`}> {d}</li>
+                        ))
+                      : "loading"}
+                  </ul>
+                </div> */}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
---
+++ src/components/services.jsx
@@ -0,0 +1,28 @@
+export const Services = (props) => {
+  return (
+    <div id="services" className="text-center">
+      <div className="container">
+        <div className="section-title">
+          <h2>Nos Services</h2>
+          <p>
+            Les brûleurs Sparxs sont conformes à la norme de sécurité NF D35-386
+          </p>
+        </div>
+        <div className="row">
+          {props.data
+            ? props.data.map((d, i) => (
+                <div key={`${d.name}-${i}`} className="col-md-4">
+                  {" "}
+                  <i className={d.icon}></i>
+                  <div className="service-desc">
+                    <h3>{d.name}</h3>
+                    <p>{d.text}</p>
+                  </div>
+                </div>
+              ))
+            : "loading"}
+        </div>
+      </div>
+    </div>
+  );
+};
